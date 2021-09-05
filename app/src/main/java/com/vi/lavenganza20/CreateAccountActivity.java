159c01b4217789533ca3f6ddbec9c69deeec2e27
vi237 <vicaerosandoval@gmail.com>
2021-09-05 19:22:25 -0400

    Toolbar

--- app/src/main/java/com/vi/lavenganza20/CreateAccountActivity.java
+++ app/src/main/java/com/vi/lavenganza20/CreateAccountActivity.java
@@ -3,6 +3,7 @@ package com.vi.lavenganza20;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        showToolBar(getString(R.string.title_crear_cuenta),true);
+    }
+    public void showToolBar(String titulo, boolean botonSubir){
+        Toolbar toolbar=findViewById(R.id.toolbar);
+       /* setSupportActionBar(toolbar);*/
+        getSupportActionBar().setTitle(titulo);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(botonSubir);
     }
 }
