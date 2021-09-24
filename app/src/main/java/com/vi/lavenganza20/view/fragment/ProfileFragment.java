21763a9f0d254bb30cc67ae710b9ac85bf0e84e5
vi237 <vicaerosandoval@gmail.com>
2021-09-24 01:37:36 -0400

    CircleImageView  SimpleViewBehavior

--- app/src/main/java/com/vi/lavenganza20/view/fragment/ProfileFragment.java
+++ app/src/main/java/com/vi/lavenganza20/view/fragment/ProfileFragment.java
@@ -2,13 +2,21 @@ package com.vi.lavenganza20.view.fragment;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.vi.lavenganza20.R;
+import com.vi.lavenganza20.adapter.CardViewAdapter;
+import com.vi.lavenganza20.model.Image;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -61,6 +69,39 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view=inflater.inflate(R.layout.fragment_profile, container, false);
+        //toolbar
+        showToolBar("",false,view);
+        //recyclerview
+        RecyclerView recyclerView=view.findViewById(R.id.recycler_view_profile);
+        //layout manager
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        //adapter
+        CardViewAdapter cardViewAdapter=new CardViewAdapter(buidImages(),R.layout.cardview_image,getActivity());
+        recyclerView.setAdapter(cardViewAdapter);
+        return view;
+    }
+    public void showToolBar(String titulo, boolean botonSubir, View view){
+        Toolbar toolbar=view.findViewById(R.id.toolbar_profile);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle(titulo);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(botonSubir);
+    }
+    //creamos lista de imagenes
+    public ArrayList<Image> buidImages(){
+        ArrayList<Image>images=new ArrayList<>();
+        images.add(new Image("https://partedeexistencia.com/wp-content/uploads/2017/02/r%C3%ADo-Torotoro-Toro-Toro-Bolivia.jpg","Carlos Valdivia","2 dias","1 me gusta"));
+        images.add(new Image("https://boliviamia.net/Images/Tourpics/atop-deathroad-02.jpg","Kim Taehyung","3 dias","2 me gusta"));
+        images.add(new Image("https://boliviamia.net/Images/Tourpics/atop-titicaca-04.jpg","Park jimin","2 dias","3 me gusta"));
+        images.add(new Image("https://tipsparatuviaje.com/wp-content/uploads/2019/07/parque-nacional-tunari.jpg","Lee Chae Rin","5 dias","4 me gusta"));
+        images.add(new Image("https://tipsparatuviaje.com/wp-content/uploads/2019/07/reserva-nacional-eduardo-abaroa.jpg","Wang Yibo","6 dias","5 me gusta"));
+        images.add(new Image("https://abarrons.com/wp-content/uploads/2019/11/Jardin-botanico-Barron-rent-a-car.jpg","Xiao Zhan","4 dias","6 me gusta"));
+        images.add(new Image("https://abarrons.com/wp-content/uploads/2019/11/Lomas-de-Arena-Barron-rent-a-Car-Destinos-turisticos.jpg","Min Yoon-gi","2 dias","7 me gusta"));
+        images.add(new Image("https://boliviamia.net/Images/Tourpics/atop-uyuni-01.jpg","Jung Ho-seok","2 dias","8 me gusta"));
+        images.add(new Image("https://javitour.com/wp-content/uploads/2018/08/lugares-m%C3%A1s-emblem%C3%A1ticos-de-Corea-del-Sur_opt-4.jpg","Park Shin-hye","6 dias","3 me gusta"));
+        images.add(new Image("https://eng.taiwan.net.tw/att/1/big_scenic_spots/pic_704_11.jpg","Bae Su-ji","9 dias","1 me gusta"));
+        return images;
     }
 }
